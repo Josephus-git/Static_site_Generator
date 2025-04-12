df9d5b80ea39f9a5c405a571b00f8d194209c64d
josephus <otejerejosephus@gmail.com>
2025-04-12 02:45:05 +0100

    trail2

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ def main():
     static = "./static"
     template_path = "./template.html"
     dest_path = "./docs"
-    print(from_path)
 
     copy_to_public(static, dest_path)
     print(generate_pages_recursive(from_path, template_path, dest_path, basepath))
